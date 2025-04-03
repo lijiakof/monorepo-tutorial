5b6e28a3db74b5b7af6789c958983da9ff8472d9
lijiakof <lijiakof@gmail.com>
2025-04-03 23:06:21 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# monorepo-tutorial
+# Monorepo 入门指南
+
+* npm init
+* npm init -w packages/utils
+* package.json
+  * "workspaces": [ "packages/*" ]
+* npm create vue@latest -w packages
+  * 请输入项目名称：app
+  * .vscode 移动到更目录
+* 更目录 package.json
+  * "scripts": { "dev": "npm run dev -workspaces" }
+* npm i
---
+++ packages/app/src/app.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/hello-world.vue'
+</script>
+
+<template>
+  <header>
+    <HelloWorld msg="You did it!" />
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
